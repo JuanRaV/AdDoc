9a7b827360be169a7bc84a5563f389f99c104349
JuanRaV <juanravirgen@gmail.com>
2023-05-03 22:31:31 -0600

    Making routing of doctors

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-
+import express from 'express'
+import doctorRoutes from './routes/doctorRoutes.js'
 
 const app = express()
 
+//Habilitando pug
+app.set('view engine', 'pug')
+app.set('views','./views')
+
+//Routing
+app.get('/',doctorRoutes)
+
+
+
+const port = 3000
+app.listen(port,()=>{
+    console.log(`Servidor corriendo en el puerto ${port}`)
+}) 
