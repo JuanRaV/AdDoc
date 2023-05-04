46cb5b5f01392f9683017877304c7ac19953d8d7
JuanRaV <juanravirgen@gmail.com>
2023-05-04 14:52:14 -0600

    Configuring database

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import doctorRoutes from './routes/doctorRoutes.js'
+import db from './config/db.js'
 
 const app = express()
 
+//Conexion a la base de datos
+try {
+    await db.authenticate()
+    console.log('Conectado a la DB')
+} catch (error) {
+    console.log(error)
+}
 //Habilitando pug
 app.set('view engine', 'pug')
 app.set('views','./views')
