affd298c189f17f6aadc12e61fb11ad5fd0a0504
Emanuel Vazquez <evazquezrosales@gmail.com>
2023-05-07 15:31:53 -0600

    rename files to better convention

---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import { formLogin,formSignup,formForgotPassword,confirm,signUp,resetPassword,checkToken,newPassword,auth } from '../controllers/authControllers.js';
+
+const router = express.Router();
+
+router.get('/login',formLogin)
+router.post('/login',auth)
+
+router.get('/signup',formSignup)
+router.post('/signup',signUp)
+
+router.get('/confirm/:token',confirm)
+
+router.get('/forget-password',formForgotPassword)
+router.post('/forget-password',resetPassword)
+
+//Almacena el nuevo password
+router.get('/forget-password/:token',checkToken)
+router.post('/forget-password/:token',newPassword)
+
+export default router 
